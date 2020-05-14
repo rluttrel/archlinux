f768eeae95a9241f9fef04bf3ead025619469bbd
rluttrel <rluttrel@hotmail.com>
2020-05-14 21:00:57 +0000

    Create archinstall.sh

---
+++ archinstall.sh
@@ -0,0 +1,7 @@
+nstall script for arch linux
+
+ping -c 2 www.archlinux.org
+yes | sudo pacman -S reflector
+sudo reflectory --verbose --latest 200  --sort rate --save /etc/pacman.d/mirrorlist
+lsblk
+(echo g; echo n; echo p; echo 1; echo ""; echo +64M; echo n; echo p; echo 2; echo ""; echo ""; echo w; echo q) | fdisk /dev/$(echo $Output_Device)
