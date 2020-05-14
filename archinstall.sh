5e2b6ba8e716bcddf2f6485ab83f44f037d5b7da
rluttrel <rluttrel@hotmail.com>
2020-05-14 23:21:48 +0000

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,19 +1,25 @@
-install script for arch linux
+install script arch linux
 
 ping -c 2 www.archlinux.org
 yes | sudo pacman -S reflector
-sudo reflector --verbose --latest 200  --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --country United States --verbose --latest 50  --sort rate --save /etc/pacman.d/mirrorlist
+sleep 10
 lsblk
+sleep 10
 (echo g; echo n; echo 1; echo ""; echo +500M; echo t; echo 1; echo n; echo 2; echo ""; echo +5G; echo n; echo 3); echo ""; echo ""; echo p; echo w | fdisk /dev/sda
+sleep 50
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mkfs.ext4 /dev/sda3
+sleep 20
 mount /dev/sda2 /mnt
 mkdir/mnt/home
 mount /dev/sda3 /mnt/home
+sleep 20
 df -h
 mkdir /mnt/etc
 genfstab -U -p /mnt >> /mnt/etc/fstab
-pacstrap -i /mnt base
+sleep 20
+yes | pacstrap -i /mnt base
 arch-chroot /mnt
-pacman -S linux linux-headers NetworkManager sudo grup efibootmgr dosfstools mtools
+yes | pacman -S linux linux-headers NetworkManager sudo grup efibootmgr dosfstools mtools
