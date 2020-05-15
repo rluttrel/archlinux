03b4b4dd4abf39dce944f4071384a0e0673e548e
rluttrel <rluttrel@hotmail.com>
2020-05-15 05:22:37 +0000

    Update archinstall2.sh

--- archinstall2.sh
+++ archinstall2.sh
@@ -1,4 +1,4 @@
-(echo 'y'; echo '1') | pacman -S linux linux-headers networkmanager sudo vi nano grub efibootmgr dosfstools mtools
+(echo 'y') | pacman -S linux linux-headers networkmanager sudo vi nano grub efibootmgr dosfstools mtools
 systemctl enable NetworkManager
 sleep 2
 mkinitcpio -p linux
