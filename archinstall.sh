a587fbe02f5dda6e9c2b8f2b413fcc514ce92504
rluttrel <rluttrel@hotmail.com>
2020-05-15 00:39:29 +0000

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -2,11 +2,11 @@ install script arch linux
 
 ping -c 2 www.archlinux.org
 yes | sudo pacman -S reflector
-sudo reflector --country United States --verbose --latest 50  --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --country "United States" --verbose --latest 50  --sort rate --save /etc/pacman.d/mirrorlist
 sleep 10
 lsblk
 sleep 10
-(echo g; echo n; echo 1; echo ""; echo +500M; echo t; echo 1; echo n; echo 2; echo ""; echo +5G; echo n; echo 3); echo ""; echo ""; echo p; echo w | fdisk /dev/sda
+(echo g; echo n; echo 1; echo ""; echo +500M; echo t; echo 1; echo n; echo 2; echo ""; echo +5G; echo n; echo 3; echo ""; echo ""; echo p; echo w | fdisk /dev/sda
 sleep 50
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
