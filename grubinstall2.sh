7886f0a3a63f877bd4bd028f8c93516fe8a0f06b
rluttrel <rluttrel@hotmail.com>
2021-07-08 16:01:15 +0000

    Update grubinstall2.sh

--- grubinstall2.sh
+++ grubinstall2.sh
@@ -13,23 +13,17 @@ sed -i 's/#en_US./en_US/g' /etc/locale.gen
 (echo ""; echo "") | pacman -S linux linux-headers networkmanager sudo vi nano grub efibootmgr dosfstools mtools
 systemctl enable dhcpd
 systemctl enable NetworkManager
-sleep 2
-#not sure if mkinitcpio is necessarry(taken care of during pacstrap command)
-#mkinitcpio -p linux
+sleep 1
 grub-install /dev/sda
-mkdir /boot/grub
-mkdir /boot/grub/locale
 grub-mkconfig -o /boot/grub/grub.cfg
 sed -i 's/#% wheel/%wheel /g' /etc/sudoers
-sleep 5
-passwd
 useradd -m  -g users -G wheel rluttrel
-passwd
+echo "Change password for rluttrel and root"
 
 #this is the next part with xorg and window NetworkManager
 #login as rluttrel
 #pacman -S base-devel git xf86-video-fbdev xorg xorg-init nitrogen picom firefox alacritty leftwm fish?
 #git clone https://aur.archlinux.org/yay-git
-makepkg -si 
-yay -S 
+#makepkg -si 
+#yay -S 
 
